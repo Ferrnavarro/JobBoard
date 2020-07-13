c7b88fe44daa1f31ce62c83b7e35ffaae363144b
ferrnavarro <fer.maganavarro@gmail.com>
2020-07-13 15:05:43 -0600

    Adding core project and entities

---
+++ JobBoard.Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobBoard.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual Guid Id { get; private set; }
+    }
+}
---
+++ JobBoard.Core/Entities/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobBoard.Core.Entities
+{
+    public class Job: BaseEntity
+    {
+        public string JobTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+
+    }
+}

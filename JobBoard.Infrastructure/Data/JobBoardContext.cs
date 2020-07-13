7a9a523a114baa2c7fc69fd645f947fc49d21a1e
ferrnavarro <fer.maganavarro@gmail.com>
2020-07-13 15:20:52 -0600

    Adding infrastructure project, context and repository

---
+++ JobBoard.Infrastructure/Data/JobBoardContext.cs
@@ -0,0 +1,18 @@
+﻿using JobBoard.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobBoard.Infrastructure.Data
+{
+    public class JobBoardContext: DbContext
+    {
+        public JobBoardContext(DbContextOptions<JobBoardContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
